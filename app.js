46a63bae55fa754b4ea5712bbd5fef131b1add19
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2024-11-04 21:23:29 +0500

    Guess GAme

---
+++ app.js
@@ -0,0 +1,12 @@
+let score = Math.floor(Math.random() * 10 );
+let usernum = prompt('Enter Your Number Between 1 To 10' );
+let remain = 3;
+for (let a = 0;a < remain.length;a++){
+    
+}
+if(score == usernum){
+    alert("YOU WIN ")
+}
+else{
+    alert(`Game Over Computer Value is ${score}`)
+}
